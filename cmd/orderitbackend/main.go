48140d5c584ae5b203d7cfaa57341089e5449999
latuan1987 <latuan1987@gmail.com>
2016-06-11 05:18:02 +0700

    Register staff

--- cmd/orderitbackend/main.go
+++ cmd/orderitbackend/main.go
@@ -14,16 +14,17 @@ import (
 var db *sql.DB = nil
 var owner string = "owner"
 var admin string = "admin"
+var waiter string = "owner"
 var staff string = "staff"
 
-type receiveJSON struct {
+type jsonRecvOwner struct {
 	OWNERNAME    string `json:"ownername"`
 	BUSINESSNAME string `json:"businessname"`
 	EMAIL        string `json:"email"`
 	PASSWORD     string `json:"password"`
 }
 
-type sendJSON struct {
+type jsonSendOwner struct {
 	OWNERNAME    string `json:"ownername"`
 	BUSINESSNAME string `json:"businessname"`
 	EMAIL        string `json:"email"`
@@ -31,20 +32,27 @@ type sendJSON struct {
 	BUSINESSID   int64  `json:"businessid"`
 }
 
+type jsonRecvStaff struct {
+	STAFFNAME     string `json:"staffName"`
+	ROLEID        int64  `json:"roleId"`
+	STAFFPASSWORD string `json:"staffPassword"`
+	BUSINESSID    int64  `json:"businessId"`
+}
+
 func register(rw http.ResponseWriter, req *http.Request) {
-	var recvjson = receiveJSON{}
+	var recvjson = jsonRecvOwner{}
 
 	body, err := ioutil.ReadAll(req.Body)
 	if err != nil {
 		log.Fatalf("Error readall: %q", err)
-		http.Error(rw, err.Error(), 500)
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
 	err = json.Unmarshal(body, &recvjson)
 	if err != nil {
 		log.Fatalf("Error decoding: %q", err)
-		http.Error(rw, err.Error(), 500)
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
@@ -54,12 +62,12 @@ func register(rw http.ResponseWriter, req *http.Request) {
 	if err == sql.ErrNoRows {
 		if err := db.QueryRow("INSERT INTO roles (name, type) VALUES ($1,$2) RETURNING id", owner, admin).Scan(&roleId); err != nil {
 			log.Fatalf("Insert new role: %q", err)
-			http.Error(rw, err.Error(), 500)
+			http.Error(rw, err.Error(), http.StatusInternalServerError)
 			return
 		}
 	} else if err != nil {
 		log.Fatalf("Error selecting roles: %q", err)
-		http.Error(rw, err.Error(), 500)
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
@@ -69,16 +77,16 @@ func register(rw http.ResponseWriter, req *http.Request) {
 	if err == sql.ErrNoRows {
 		if err := db.QueryRow("INSERT INTO businesses (name, address) VALUES ($1,$2) RETURNING id", recvjson.BUSINESSNAME, "").Scan(&businessId); err != nil {
 			log.Fatalf("Insert new businesses: %q", err)
-			http.Error(rw, err.Error(), 500)
+			http.Error(rw, err.Error(), http.StatusInternalServerError)
 			return
 		}
 	} else if err != nil {
 		log.Fatalf("Error selecting businesses: %q", err)
-		http.Error(rw, err.Error(), 500)
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
 		return
 	} else {
 		log.Fatal("Already exist business name")
-		http.Error(rw, "Already exist business name", 500)
+		http.Error(rw, "Already exist business name", http.StatusInternalServerError)
 		return
 	}
 
@@ -92,15 +100,15 @@ func register(rw http.ResponseWriter, req *http.Request) {
 		}
 	} else if err != nil {
 		log.Fatalf("Error selecting businesses: %q", err)
-		http.Error(rw, err.Error(), 500)
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
 		return
 	} else {
 		log.Fatal("Already exist owner name")
-		http.Error(rw, "Already exist owner name", 500)
+		http.Error(rw, "Already exist owner name", http.StatusInternalServerError)
 		return
 	}
 
-	sendData := sendJSON{
+	sendData := jsonSendOwner{
 		OWNERNAME:    recvjson.OWNERNAME,
 		BUSINESSNAME: recvjson.BUSINESSNAME,
 		EMAIL:        recvjson.EMAIL,
@@ -110,12 +118,66 @@ func register(rw http.ResponseWriter, req *http.Request) {
 	encoder, err := json.Marshal(sendData)
 	if err != nil {
 		log.Fatalf("Error marshal: %q", err)
-		http.Error(rw, err.Error(), 500)
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
 	rw.Header().Set("Content-Type", "application/json")
 	rw.Write(encoder)
+}
+
+func registerStaff(rw http.ResponseWriter, req *http.Request) {
+	var jsonStaff = jsonRecvStaff{}
+
+	body, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		log.Fatalf("Error readall: %q", err)
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = json.Unmarshal(body, &jsonStaff)
+	if err != nil {
+		log.Fatalf("Error decoding: %q", err)
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Insert role if not exist
+	var roleId int64 = 0
+	err = db.QueryRow("SELECT id FROM roles WHERE id=$1", owner).Scan(&roleId)
+	if err == sql.ErrNoRows {
+		if err := db.QueryRow("INSERT INTO roles (name, type) VALUES ($1,$2) RETURNING id", waiter, staff).Scan(&roleId); err != nil {
+			log.Fatalf("Insert new role: %q", err)
+			http.Error(rw, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	} else if err != nil {
+		log.Fatalf("Error selecting roles: %q", err)
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Insert to users
+	var userId int64 = 0
+	err = db.QueryRow("SELECT id FROM users WHERE username=$1", jsonStaff.STAFFNAME).Scan(&userId)
+	if err == sql.ErrNoRows {
+		if err := db.QueryRow("INSERT INTO users (username, password, email, business_id, role_id) VALUES ($1,$2,$3,$4,$5) RETURNING id", jsonStaff.STAFFNAME, jsonStaff.STAFFPASSWORD, "", jsonStaff.BUSINESSID, jsonStaff.ROLEID).Scan(&userId); err != nil {
+			log.Fatalf("Insert new users: %q", err)
+			return
+		}
+	} else if err != nil {
+		log.Fatalf("Error selecting businesses: %q", err)
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+		return
+	} else {
+		log.Fatal("Already exist owner name")
+		http.Error(rw, "Already exist owner name", http.StatusInternalServerError)
+		return
+	}
+
+	rw.WriteHeader(http.StatusOK)
+	rw.Write([]byte("true"))
 }
 
 func main() {
@@ -157,6 +219,7 @@ func main() {
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/register", register)
+	mux.HandleFunc("/registerStaff", registerStaff)
 
 	// cors.Default() setup the middleware with default options being
 	// all origins accepted with simple methods (GET, POST). See
